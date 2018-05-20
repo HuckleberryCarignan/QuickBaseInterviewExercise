ac8a6d298e5a3abec2d5e24775a5cb678c01da77
huckleberrycarignan <MoxieSince1884>
2018-05-20 10:46:38 -0400

    Initial Commit

---
+++ src/test/java/QuickBaseTest.java
@@ -0,0 +1,29 @@
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class QuickBaseTest {
+
+    //@DataProvider(name="twoLargestValuesTestInputData")
+
+    public Object[][] twoLargestValuesTestInputDataObject() {
+            return new Object[][]{
+                [2,2];
+        }
+    }
+
+    @Test
+    public void testTwoLargest() {
+        QuickBase twoLargestObject = new QuickBase();
+        List<Integer> testList = new ArrayList<>();
+//        testList.add(10);
+        Collections.addAll(testList,3,1,5,2,0);
+        twoLargestObject.twoLargest(testList);
+        Assert.assertEquals(twoLargestObject.twoLargest(testList),1);
+        System.out.println("Running Test -> testTwoLargest");
+    }
+}
